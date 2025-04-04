aed8bd7a4e6641b8d9c2225035cd3501e7bfa032
Alok Verma <alokvema0122@gmail.com>
2025-04-04 17:33:05 -0500

    Toggle button using the useContext Api

--- panda/src/App.js
+++ panda/src/App.js
@@ -1,10 +1,21 @@
+import { createContext, useState } from 'react';
 import './App.css';
+import { Form } from './components/Form';
+export const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-   <div>
-    <h3>Hello Everyone</h3>
-   </div>
+    <div className={theme}>
+      <ThemeContext.Provider value={theme}>
+        <Form/>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </ThemeContext.Provider>
+    </div>
   );
 }
 
