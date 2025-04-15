affb2c4b19093371945ae0177e52e88370c298ae
Alok Verma <alokvema0122@gmail.com>
2025-04-15 23:28:11 +0530

    Feature add in Todo App

--- src/components/TodoData.js
+++ src/components/TodoData.js
@@ -1,95 +1,94 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { MdDeleteForever } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
+import { HiOutlineSave } from "react-icons/hi";
 
-const TodoData = ({ tasks, setTasks }) => {
-    const [input, setInput] = useState('');
-    const [edit, setEdit] = useState(null);
-    const [completed, setCompleted] = useState(Array(tasks.length).fill(false));
+const TodoData = ({ tasks, setTasks,filterTodos }) => {
+  const [input, setInput] = useState('');
+  const [edit, setEdit] = useState(null);
 
-    const deleteHandler = (id) => {
-        const newTodos = [...tasks];
-        const newCompleted = [...completed];
-        newTodos.splice(id, 1);
-        newCompleted.splice(id, 1);
-        setTasks(newTodos);
-        localStorage.setItem("localTasks", JSON.stringify(newTodos))
-        setCompleted(newCompleted);
+  const deleteHandler = (id) => {
+    const updatedTasks = tasks.filter((_, index) => index !== id);
+    setTasks(updatedTasks);
+    localStorage.setItem("localTasks", JSON.stringify(updatedTasks));
+  };
 
-    };
+  const editHandler = (id) => {
+    if (!tasks[id].completed) {
+      setEdit(id);
+      setInput(tasks[id].text);
+    }
+  };
 
-    const editHandler = (id) => {
-        if (!completed[id]) {
-            setEdit(id);
-            setInput(tasks[id]);
-        }
-    };
+  const saveHandler = () => {
+    const updatedTasks = [...tasks];
+    updatedTasks[edit].text = input;
+    setTasks(updatedTasks);
+    localStorage.setItem("localTasks", JSON.stringify(updatedTasks));
+    setEdit(null);
+    setInput('');
+  };
 
-    const saveHandler = () => {
-        const updatedTasks = [...tasks];
-        updatedTasks[edit] = input;
-        setTasks(updatedTasks);
-        localStorage.setItem("localTasks", JSON.stringify(updatedTasks))
-        setEdit(null);
-        setInput('');
-    };
+  const toggleComplete = (id) => {
+    const updatedTasks = [...tasks];
+    	console.log(updatedTasks[id]);
+    updatedTasks[id].completed = !updatedTasks[id].completed;
+    setTasks(updatedTasks);
+    console.log(updatedTasks)
+    localStorage.setItem("localTasks", JSON.stringify(updatedTasks));
+  };
 
-    const toggleComplete = (id) => {
-        const updateCompleted = [...completed];
-        updateCompleted[id] = !updateCompleted[id];
-        setCompleted(updateCompleted);
+  return (
+    <div>
+      <ul>
+        {filterTodos.map((task, id) => (
+          <div className="task-item" key={id}>
+                <input
+              className='round-checkbox'
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleComplete(id)}
+            />          
+            {edit === id ? (
+              <input
+                className="edit-box"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onBlur={saveHandler}
+                autoFocus
+              />
+            ) : (
+              <span className={task.completed ? 'completed-task' : ""}>
+                {task.text}
+              </span>
+            )}
 
-    };
-    return (
-        <div>
-            <ul>
-                {tasks.map((task, id) => (
-                    <div className="task-item" key={id}>
-                        <input
-                            type="checkbox"
-                            checked={completed[id]}
-                            onChange={() => toggleComplete(id)}
-                        />
-                        {edit === id ? (
-                            <input
-                                className="edit-box"
-                                type="text"
-                                value={input}
-                                onChange={(e) => setInput(e.target.value)}
-                                onBlur={saveHandler}
-                            />
-                        ) : (
-                            <span className={completed[id] ? 'completed-task' : ""}>
-                                {task}
-                            </span>
-                        )}
-
-                        <div className='btn-div'>
-                            {edit === id ? (
-                            <button className="save-btn" onClick={saveHandler}>
-                               Save
-                             </button>
-                            ) : (
-                            <>
-                            <button className="delete-btn" onClick={() => deleteHandler(id)}>
-                            <MdDeleteForever size={20} />
-                             </button>
-                            <button
-                            className="edit-todo"
-                            onClick={() => editHandler(id)}
-                            disabled={completed[id]}
-                            >
-                            <FaRegEdit size={20} />
-                    </button>
-        </>
-    )}
-</div>
-
-                    </div>
-                ))}
-            </ul>
-        </div>
-    );
+            <div className='btn-div'>
+              {edit === id ? (
+                <button className="save-btn" onClick={saveHandler}>
+                  <HiOutlineSave size={22} />
+                </button>
+              ) : (
+                <>
+                  <button
+                    className="edit-todo"
+                    onClick={() => editHandler(id)}
+                    disabled={task.completed}
+                  >
+                    <FaRegEdit size={20} />
+                  </button>
+                  <button className="delete-btn" onClick={() => deleteHandler(id)}>
+                    <MdDeleteForever size={20} />
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default TodoData; 
+export default TodoData;
