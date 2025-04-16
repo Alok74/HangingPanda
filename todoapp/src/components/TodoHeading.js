2d5feb38f2227fb4e56294e99cf9b73cccb80254
Alok Verma <alokvema0122@gmail.com>
2025-04-16 19:52:37 +0530

    Created todo app with redux

--- todoapp/src/components/TodoHeading.js
+++ todoapp/src/components/TodoHeading.js
@@ -6,7 +6,7 @@ const TodoHeading = () => {
     <>
     <div className=' ml-[310px] mr-auto'>  
     <h1 className='text-5xl text-center ml-40 pt-10 mb-5 bg-green-950 h-[120px] text-white
-    w-[600px] m-auto '>Todo App</h1>
+    w-[600px] m-auto '>Todo App1</h1>
     </div>
     <TodoWithRedux/>
     </>
